b02b7acbdf6b46a583bfd00a857e0cc077b25f2e
Tukashov <erbol.tukashov.kk@gmail.com>
2022-02-02 20:00:34 +0600

    feat: add layout and done real estate properties

--- components/Property.jsx
+++ components/Property.jsx
@@ -14,6 +14,26 @@ const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, 
             <Box>
                 <Image src={coverPhoto ? coverPhoto.url : defaultImage} width={400} height={260} alt='house photo'/>
             </Box>
+            <Box w="full">
+                <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+                    <Flex alignItems="center">
+                        <Box paddingRight="3" color="green.400">
+                            {isVerified && <GoVerified/>}
+                        </Box>
+                        <Text fontWeight="bold" fontSize="lg">AED {millify(price)} {rentFrequency && `/${rentFrequency}`}</Text>
+                    </Flex>
+
+                    <Box>
+                        <Avatar size="sm" src={agency?.logo?.url}/>
+                    </Box>
+                </Flex>
+                <Flex alignItems="center" justifyContent="space-between" p="1" w="250px" color="blue.400">
+                    {rooms} <FaBath/> | {baths} <FaBath/> | {millify(area)} sqft <BsGridFill/>
+                </Flex>
+                <Text  fontSize="lg">
+                    {title.lenght > 30 ? `${title.substrings(0, 30)}...` : title}
+                </Text>
+            </Box>
         </Flex>
     </Link>
 )
