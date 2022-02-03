147f7047be6ffa81b1264070c463a9b99c4a62a7
Tukashov <erbol.tukashov.kk@gmail.com>
2022-02-03 20:04:17 +0600

    faet: add search page

---
+++ components/SearchFilter.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import { Flex, Box, Select, Text, Input, Spinner, Icon, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { MdCencel } from "react-icons/md";
+import Image from "next/image";
+
+
+const SearchFilter = () => {
+    return (
+        <div>
+            search filter
+        </div>
+    )
+}
+
+export default SearchFilter;
