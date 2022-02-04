8fa495554acba8f229f4a796bb857befb7815e7e
Tukashov <erbol.tukashov.kk@gmail.com>
2022-02-04 23:28:13 +0600

    faet: add property details

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,33 @@
 import Router from 'next/router';
 import Head from 'next/head';
-import NProgress from 'progress';
+import NextNProgress from 'nprogress';
 import { ChakraProvider } from '@chakra-ui/react';
+
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
+  NextNProgress.configure({ showSpinner: false });
+
+  Router.events.on('routeChangeStart', () => {
+    NextNProgress.start();
+  });
+
+  Router.events.on('routeChangeComplete', () => {
+    NextNProgress.done();
+  });
+
   return (
     <>
       <Head>
-
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
       </Head>
       <ChakraProvider>
         <Layout>
-            <Component {...pageProps} />
+          <Component {...pageProps} />
         </Layout>
       </ChakraProvider>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
